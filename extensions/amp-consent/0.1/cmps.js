ed37978ccdca3de5a5bc37366e7571432955aae2
ptrickney <pwhitney@onetrust.com>
2020-07-27 12:22:16 -0400

    add onetrust cmp

--- extensions/amp-consent/0.1/cmps.js
+++ extensions/amp-consent/0.1/cmps.js
@@ -66,6 +66,12 @@ CMP_CONFIG['Ogury'] = {
   'promptUISrc': 'https://www.ogury.mgr.consensu.org/amp.html',
 };
 
+CMP_CONFIG['onetrust'] = {
+  'consentInstanceId': 'onetrust',
+  'checkConsentHref': '/get-consent-v1',  
+  'promptUISrc': 'https://onetoso.com/PSOTesting/PatrickTester/EnhancementsAndFixes/Enhancements/AmpCmp/amp-iframe.html',
+};
+
 CMP_CONFIG['opencmp'] = {
   'consentInstanceId': 'opencmp',
   'checkConsentHref': 'https://amp.opencmp.net/consent/check',
